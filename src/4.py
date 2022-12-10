ad4c3a679a7f294f4ca59a15aafe4a83252e9018
Dave Briccetti <daveb@davebsoft.com>
2022-12-10 10:12:47 -0800

    Day 4 part 2

--- src/4.py
+++ src/4.py
@@ -1,20 +1,26 @@
 from shared import get_lines
 
-class ORange:
+class Range:
     def __init__(self, range_string: str):
         self.start, self.stop = [int(n) for n in range_string.split('-')]
 
     def __repr__(self):
         return f'{self.start}-{self.stop}'
 
-    def contains(self, other: 'ORange') -> bool:
+    def contains(self, other: 'Range') -> bool:
         return other.start >= self.start and other.stop <= self.stop
+
+    def overlaps(self, other: 'Range') -> bool:
+        return self.start <= other.start <= self.stop or self.start <= other.stop <= self.stop
 
 lines = get_lines('../data/4.txt')
 
 num_contained = 0
+num_overlaps = 0
 for line in lines:
-    r1, r2 = [ORange(r) for r in line.split(',')]
+    r1, r2 = [Range(r) for r in line.split(',')]
     if r1.contains(r2) or r2.contains(r1):
         num_contained += 1
-print(num_contained)
+    if r1.overlaps(r2) or r2.overlaps(r1):
+        num_overlaps += 1
+print(f'{num_contained=}, {num_overlaps=}')

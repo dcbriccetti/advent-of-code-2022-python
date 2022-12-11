ef235b6565df19dccba4a299a6393f34c3668f67
Dave Briccetti <daveb@davebsoft.com>
2022-12-11 11:06:39 -0800

    Day 7 part 2

--- src/7.py
+++ src/7.py
@@ -13,7 +13,6 @@ class Dir:
         tot = 0
         for child in self.children:
             tot += (child.size(collector) if isinstance(child, Dir) else child.size)
-        print(f'size of {self.name}: {tot}')
         collector(tot)
         return tot
 
@@ -23,36 +22,48 @@ class File:
     size: int
     parent: Dir
 
-lines = get_lines('../data/7.txt')
-root_dir = Dir('/')
-current_dir = root_dir
+def build_directory_structure():
+    root_dir = Dir('/')
+    current_dir = root_dir
+    for line in get_lines('../data/7.txt'):
+        parts = line.split()
+        if line == '$ cd /':
+            current_dir = root_dir
+        elif line == '$ cd ..':
+            current_dir = current_dir.parent
+        elif line.startswith('$ cd '):
+            current_dir = \
+            [child for child in current_dir.children if isinstance(child, Dir) and child.name == parts[2]][0]
+        elif line == '$ ls':
+            pass
+        elif parts[0] == 'dir':
+            current_dir.children.append(Dir(parts[1], current_dir))
+        elif parts[0].isnumeric():
+            current_dir.children.append(File(parts[1], int(parts[0]), current_dir))
+        else:
+            print('oops')
+    return root_dir
 
-for line in lines:
-    parts = line.split()
-    print(line)
-    if line == '$ cd /':
-        current_dir = root_dir
-    elif line == '$ cd ..':
-        current_dir = current_dir.parent
-    elif line.startswith('$ cd '):
-        for child in current_dir.children:
-            if isinstance(child, Dir) and child.name == parts[2]:
-                current_dir = child
-                break
-    elif line == '$ ls':
-        pass
-    elif parts[0] == 'dir':
-        current_dir.children.append(Dir(parts[1], current_dir))
-    elif parts[0].isnumeric():
-        current_dir.children.append(File(parts[1], int(parts[0]), current_dir))
-    else:
-        print('oops')
+def compute_results():
+    class Collector:
+        def __init__(self):
+            self.sum_of_sizes = 0
+            self.sizes: list[int] = []
 
-sum_of_sizes = 0
-def collector(size: int):
-    global sum_of_sizes
-    if size <= 100_000:
-        sum_of_sizes += size
+        def collect(self, size: int):
+            self.sizes.append(size)
+            if size <= 100_000:
+                self.sum_of_sizes += size
 
-print(root_dir.size(collector))
-print(sum_of_sizes)
+    collector = Collector()
+    print(f'{root_dir.size(collector.collect)=}')
+    print(f'{collector.sum_of_sizes=}')
+    collector.sizes.sort()
+    unused_space = 70_000_000 - collector.sizes[-1]
+    needed_space = 30_000_000 - unused_space
+    print(f'{unused_space=}, {needed_space=}')
+    candidates = [s for s in collector.sizes if s >= needed_space]
+    print(f'{candidates[0]=}')
+
+root_dir = build_directory_structure()
+compute_results()

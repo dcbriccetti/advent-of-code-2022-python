10de060fbc674e7b57a30f80a480075ed5daa1ae
Dave Briccetti <daveb@davebsoft.com>
2023-01-02 12:44:05 -0800

    Improve Day 3

--- src/3.py
+++ src/3.py
@@ -1,37 +1,35 @@
+from typing import Iterable
 from shared import get_lines
 
-sacks = get_lines('../data/3.txt')
-
-def part1():
+def part1() -> None:
     print(sum(find_sack_priority(sack) for sack in sacks))
 
-def part2():
+def part2() -> None:
     print(sum(find_group_priorities()))
 
-def groups():
+def groups() -> Iterable[str]:
     for i in range(0, len(sacks), 3):
         yield sacks[i:i+3]
 
-def find_group_priorities():
+def find_group_priorities() -> Iterable[int]:
     for group in groups():
         group_sets = [set(sack) for sack in group]
         char_in_common = list(group_sets[0] & group_sets[1] & group_sets[2])[0]
         yield find_char_priority(char_in_common)
 
-def find_sack_priority(sack):
+def find_sack_priority(sack: str) -> int:
     half_len = len(sack) // 2
-    compartment1 = sack[:half_len]
-    compartment2 = sack[half_len:]
+    compartment1, compartment2 = sack[:half_len], sack[half_len:]
     compartment1_chars = set(compartment1)
-    in_both: str = [c for c in compartment2 if c in compartment1_chars][0]
-    priority = find_char_priority(in_both)
-    return priority
+    char_in_both: str = [c for c in compartment2 if c in compartment1_chars][0]
+    return find_char_priority(char_in_both)
 
 
-def find_char_priority(in_both: str) -> int:
-    sub_val, offset = ('a', 0) if in_both.islower() else ('A', 26)
-    priority = ord(in_both) - ord(sub_val) + 1 + offset
-    return priority
+def find_char_priority(char: str) -> int:
+    subtract_val, offset = ('a', 0) if char.islower() else ('A', 26)
+    return ord(char) - ord(subtract_val) + 1 + offset
+
+sacks: list[str] = get_lines('../data/3.txt')
 
 part1()
 part2()
